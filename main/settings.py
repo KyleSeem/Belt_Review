c88f149f5be05b3add2e3eb5aaaeee2851e1b68c
Kyle Seem <kyle.seem@gmail.com>
2018-05-29 14:40:29 -0400

    added untracked changes

--- main/settings.py
+++ main/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.book_reviews',
+    'apps.login_reg',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
